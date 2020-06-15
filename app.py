071bc11a900d716d0024eef1bb6f180ca494e9fd
Ammar sahyoun <ammarzgr@hotmail.com>
2020-06-15 23:36:22 +0200

    Test and clean

--- app.py
+++ app.py
@@ -1,145 +1,78 @@
 import streamlit as st
 import pandas as pd
+import mushromm
 import about
-import streamlit as st
-import pandas as pd
-import about
-import numpy as np
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
-from sklearn.metrics import precision_score, recall_score
+import UploadFile
 from PIL import Image
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
+
 
 def main():
-    st.success("# _Model Hints Web App_ `version0.0.1` ")
-    st.title("Binary Problem Classifier")
+    menu = ['Mushroom data', 'About', 'Upload dataset']
+    menuSelection = st.sidebar.radio('', menu, index=1, key=None)
+
+    if menuSelection == 'About':
+        header()
+        about.display_about()
+        about.display_contribute()
+        if st.sidebar.button("Many thanks"):
+            st.balloons()
+
+    if menuSelection == 'Upload dataset':
+        st.success("# _Model Hints_Web App_ `version0.0.1` ")
+        st.title("3-Models of Binary Classification")
+        st.header(" **📁Uplaod CSV file**")
+        uploadFile()
+
+
+
+    if menuSelection == 'Mushroom data':
+        header()
+        mushromm.mushroomSetup()
+
+def header():
+    st.success("# _Model Hints_Web App_ `version0.0.1` ")
+    st.title("3-Models of Binary Classification")
     img = Image.open("./templates/mushroom.jpg")
     st.image(img)
     st.header(" **🍄Are your mushrooms edible or poisonous?🍄**")
 
-    menu = ['Mushroom data', 'About', 'Upload dataset']
-    menuSelection = st.sidebar.radio('', menu, index=1, key=None)
-    if menuSelection == 'About':
-        about.display_about()
-        about.display_contribute()
-    if menuSelection == 'Upload dataset':
-        pass
-    if menuSelection == 'Mushroom data':
-        @st.cache(persist=True)
-        def load_data():
-            data = pd.read_csv("./data/mushrooms.csv")
-            labelencoder = LabelEncoder()
-            for col in data.columns:
-                data[col] = labelencoder.fit_transform(data[col])
-            return data
+def uploadFile():
+    uploaded_file = st.file_uploader("", type="csv")
+    if uploaded_file is not None:
+        dataFrame = pd.read_csv(uploaded_file)
 
-        @st.cache(persist=True)
-        def split(df):
-            y = df.type
-            X = df.drop(columns=['type'])
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-            return X_train, X_test, y_train, y_test
+    try:
+        EA = UploadFile.FileInfo(dataFrame)
+        st.success('Successfully uploaded!')
 
-        def plot_metrics(metrics_list):
-            if 'Confusion Matrix' in metrics_list:
-                st.subheader("Confusion Matrix")
-                plot_confusion_matrix(model, X_test, y_test, display_labels=class_names)
-                st.pyplot()
+        st.title('Dataframe basic informations')
 
-            if 'ROC Curve' in metrics_list:
-                st.subheader("ROC Curve")
-                plot_roc_curve(model, X_test, y_test)
-                st.pyplot()
+        st.sidebar.subheader('Basica exploratory analysis options')
+        if st.sidebar.checkbox('Basic informations'):
 
-            if 'Precision-Recall Curve' in metrics_list:
-                st.subheader('Precision-Recall Curve')
-                plot_precision_recall_curve(model, X_test, y_test)
-                st.pyplot()
+            if st.sidebar.checkbox('Head'):
+                st.subheader('Dataframe head:')
+                st.write(dataFrame.head())
 
-        df = load_data()
-        class_names = ['edible', 'poisonous']
-        X_train, X_test, y_train, y_test = split(df)
-        st.sidebar.subheader("Choose Classifier")
-        classifier = st.sidebar.selectbox("",
-                                          ("Support Vector Machine (SVM)", "Logistic Regression", "Random Forest"))
-        
-        if st.sidebar.checkbox("Feature Scaling", False):
-            sc = StandardScaler()
-            X_train = sc.fit_transform(X_train)
-            X_test = sc.transform(X_test)
+            if st.sidebar.checkbox('Describe'):
+                st.subheader('Dataframe description:')
+                st.write(dataFrame.describe())
 
-        if st.checkbox("Show raw data", False):
-            st.subheader("Mushroom DataSet (8124, 23) used for classification problem")
-            st.write(df)
-            st.markdown(
-                "This [data set](https://archive.ics.uci.edu/ml/datasets/Mushroom) includes descriptions of hypothetical samples corresponding to 23 species of gilled mushrooms "
-                "in the Agaricus and Lepiota Family (pp. 500-525). Each species is identified as definitely edible, definitely poisonous, "
-                "or of unknown edibility and not recommended. This latter class was combined with the poisonous one.")
+            if st.sidebar.checkbox('Info'):
+                st.subheader('Dataframe informations:')
+                st.text(EA.info())
 
-        if classifier == 'Support Vector Machine (SVM)':
-            st.sidebar.subheader("Model Hyperparameters")
-            C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_SVM')
-            kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key='kernel')
-            gamma = st.sidebar.radio("Gamma (Kernel Coefficient)", ("scale", "auto"), key='gamma')
+            if st.sidebar.checkbox('Isnull'):
+                st.subheader('Null occurrences')
+                st.write(dataFrame.isnull().sum())
 
-            st.sidebar.subheader("Choose a metric to plot:")
-            metrics = st.sidebar.multiselect("",
-                                             ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
-            if st.sidebar.button("Classify", key='classify'):
-                st.subheader("Support Vector Machine (SVM) Results")
-                model = SVC(C=C, kernel=kernel, gamma=gamma)
-                model.fit(X_train, y_train)
-                accuracy = model.score(X_test, y_test)
-                y_pred = model.predict(X_test)
-                st.write("Model accuracy: ", accuracy.round(2))
-                st.write("Model precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
-                st.write("Model recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
-                plot_metrics(metrics)
+            if st.sidebar.checkbox('Unique values and frequency'):
+                col = st.sidebar.selectbox('Choose a column for see unique values', EA.columns)
+                st.subheader('Unique values and frequency')
+                st.write(EA.info2(col))
 
-        if classifier == 'Logistic Regression':
-            st.sidebar.subheader("Model Hyperparameters")
-            C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_LR')
-            max_iter = st.sidebar.slider("Maximum number of iterations", 100, 500, key='max_iter')
-
-            metrics = st.sidebar.multiselect("What metrics to plot?",
-                                             ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
-
-            if st.sidebar.button("Classify", key='classify'):
-                st.subheader("Logistic Regression Results")
-                model = LogisticRegression(C=C, penalty='l2', max_iter=max_iter)
-                model.fit(X_train, y_train)
-                accuracy = model.score(X_test, y_test)
-                y_pred = model.predict(X_test)
-                st.write("Model accuracy: ", accuracy.round(2))
-                st.write("Model precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
-                st.write("Model recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
-                plot_metrics(metrics)
-
-        if classifier == 'Random Forest':
-            st.sidebar.subheader("Model Hyperparameters")
-            n_estimators = st.sidebar.number_input("The number of trees in the forest", 100, 5000, step=10,
-                                                   key='n_estimators')
-            max_depth = st.sidebar.number_input("The maximum depth of the tree", 1, 20, step=1, key='n_estimators')
-            bootstrap = st.sidebar.radio("Bootstrap samples when building trees", ('True', 'False'), key='bootstrap')
-            metrics = st.sidebar.multiselect("What metrics to plot?",
-                                             ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
-
-            if st.sidebar.button("Classify", key='classify'):
-                st.subheader("Random Forest Results")
-                model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, bootstrap=bootstrap,
-                                               n_jobs=-1)
-                model.fit(X_train, y_train)
-                accuracy = model.score(X_test, y_test)
-                y_pred = model.predict(X_test)
-                st.write("Model accuracy: ", accuracy.round(2))
-                st.write("Model precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
-                st.write("Model recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
-                plot_metrics(metrics)
+    except:
+        st.error('Upload a CSV file to get started.')
 
 
 if __name__ == '__main__':
